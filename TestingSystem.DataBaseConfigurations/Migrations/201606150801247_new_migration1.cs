0570975298b92d7c2aae0a0cae55292deb2b84a0
Іван Прусак <john_prusak@mail.ru>
2016-06-16 00:21:10 +0300

    Some frames commit (not all frames)

---
+++ TestingSystem.DataBaseConfigurations/Migrations/201606150801247_new_migration1.cs
@@ -0,0 +1,16 @@
+namespace TestingSystem.DataBaseConfigurations.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class new_migration1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
