769a2843c5fac6043570dbfb4090e39f26cedb4c
Іван Прусак <john_prusak@mail.ru>
2016-06-16 19:49:52 +0300

    Booblyk's first sprint is over

---
+++ TestingSystem.DataBaseConfigurations/Migrations/201606161511121_new_mo=igration_for_check.cs
@@ -0,0 +1,16 @@
+namespace TestingSystem.DataBaseConfigurations.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class new_moigration_for_check : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ TestingSystem.Web/App_Start/UnityWebapiConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using Microsoft.Practices.ObjectBuilder2;
+using TestingSystem.Services.Interfaces;
+using TestingSystem.Services.Implementation;
+using TestingSystem.DataBaseConfigurations.Infrastructure;
+using TestingSystem.DataBaseConfigurations.Repositories;
+using TestingSystem.DataBaseConfigurations.Extension;
+using TestingSystem.Entities;
+
+namespace TestingSystem.Web.App_Start
+{
+    public class UnityWebapiConfig
+    {
+        public static UnityContainer Сontainer { get; set; }
+        
+        public static void RegisterComponents()
+        {
+            Сontainer = new UnityContainer();
+
+            Сontainer.RegisterType<IUserService, UserService>();
+
+            Сontainer.RegisterType<IUnitOfWork, UnitOfWork>();
+
+            Сontainer.RegisterType<IRepository<User>, Repository<User>>();
+
+            Сontainer.RegisterType<IDbProvide, DbProvide>();
+        }
+
+      
+
+    }
+}
