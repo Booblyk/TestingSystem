d0677226d839604baf6e42e74f4d5c0a372df0b1
Іван Прусак <john_prusak@mail.ru>
2016-06-16 20:26:25 +0300

    Additional commit

---
+++ TestingSystem.Services/Implementation/UserService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingSystem.Entities;
+using TestingSystem.DataBaseConfigurations.Repositories;
+using TestingSystem.DataBaseConfigurations.Infrastructure;
+using TestingSystem.DataBaseConfigurations.Extension;
+
+namespace TestingSystem.Services.Implementation
+{
+    public class UserService : Interfaces.IUserService
+    {
+        IRepository<User> _userRepository;
+        IUnitOfWork _unitOfWork;
+        public UserService(IRepository<User> userRepos, IUnitOfWork unitOfWork)
+        {
+            _userRepository = userRepos;
+            _unitOfWork = unitOfWork;
+        }
+        
+        public User CreateUser(string email, string password)
+        {
+            var existingUser = _userRepository.GetById(email); //має бути GetBeEmail
+
+            if (existingUser != null)
+            {
+                throw new Exception("Email is already in use");
+            }
+
+            var user = new User()   //тут змінити треба!!!
+            {
+                PasswordSalt = "",
+                Email = email,
+               PasswordHash=password
+            };
+
+            _userRepository.Add(user);
+
+            _unitOfWork.Commit();
+
+            return user;
+
+        }
+
+        public User GetUser(int UserId)
+        {
+            return _userRepository.GetSingle(UserId);
+        }
+    }
+}
---
+++ TestingSystem.Services/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingSystem.Entities;
+
+
+namespace TestingSystem.Services.Interfaces
+{
+    public  interface IUserService
+    {
+        User GetUser(int UserId);
+        User CreateUser(string email, string password);
+    }
+}
